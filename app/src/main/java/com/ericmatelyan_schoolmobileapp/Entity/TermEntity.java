6dd5fdb6e55e3915c14439af565bdf098b41912d
samurainstein <eric.matelyan@gmail.com>
2021-08-27 17:45:37 -0400

    Setup term entity class.  Constructors, getters, setters, and toString()

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/Entity/TermEntity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/Entity/TermEntity.java
@@ -1,4 +1,66 @@
 package com.ericmatelyan_schoolmobileapp.Entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity (tableName = "term_table")
 public class TermEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int termId;
+    private String termName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public TermEntity(int termId, String termName, LocalDate startDate, LocalDate endDate) {
+        this.termId = termId;
+        this.termName = termName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getTermId() {
+        return termId;
+    }
+
+    public void setTermId(int termId) {
+        this.termId = termId;
+    }
+
+    public String getTermName() {
+        return termName;
+    }
+
+    public void setTermName(String termName) {
+        this.termName = termName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "TermEntity{" +
+                "termId=" + termId +
+                ", termName='" + termName + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
 }
+
+
