7b7e22be225efddf47486f4a3dabfc00ab329f94
samurainstein <eric.matelyan@gmail.com>
2021-08-27 17:45:53 -0400

    Setup Course entity class.  Constructors, getters, setters, and toString()

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/Entity/CourseEntity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/Entity/CourseEntity.java
@@ -1,4 +1,109 @@
 package com.ericmatelyan_schoolmobileapp.Entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity (tableName = "course_table")
 public class CourseEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int courseId;
+    private String courseName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
+    private String instructorName;
+    private String instructorPhone;
+    private String instructorEmail;
+
+    public CourseEntity(int courseId, String courseName, LocalDate startDate, LocalDate endDate, String status,
+                        String instructorName, String instructorPhone, String instructorEmail) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.instructorName = instructorName;
+        this.instructorPhone = instructorPhone;
+        this.instructorEmail = instructorEmail;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorPhone() {
+        return instructorPhone;
+    }
+
+    public void setInstructorPhone(String instructorPhone) {
+        this.instructorPhone = instructorPhone;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseEntity{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", status='" + status + '\'' +
+                ", instructorName='" + instructorName + '\'' +
+                ", instructorPhone='" + instructorPhone + '\'' +
+                ", instructorEmail='" + instructorEmail + '\'' +
+                '}';
+    }
 }
