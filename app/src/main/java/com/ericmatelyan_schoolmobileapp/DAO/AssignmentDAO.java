d9a980b1aa561fe5da8d879d470e4ca231326653
samurainstein <eric.matelyan@gmail.com>
2021-08-28 10:07:18 -0400

    Setup AssignmentDAO class.  Create insert, update, delete, and select queries.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/AssignmentDAO.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/AssignmentDAO.java
@@ -1,4 +1,29 @@
 package com.ericmatelyan_schoolmobileapp.DAO;
 
+import static androidx.room.OnConflictStrategy.IGNORE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
+
+import java.util.List;
+
+@Dao
 public interface AssignmentDAO {
+    @Insert(onConflict = IGNORE)
+    void insert(AssignmentEntity assignment);
+
+    @Update
+    void update(AssignmentEntity assignment);
+
+    @Delete
+    void delete(AssignmentEntity assignment);
+
+    @Query("SELECT * FROM assignment_table")
+    List<AssignmentEntity> getAllAssignments();
 }
