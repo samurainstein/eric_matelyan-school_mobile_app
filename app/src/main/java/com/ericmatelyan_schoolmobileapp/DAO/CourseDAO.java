76148eefeeefcffda487e00d1724fd5ddba9cf46
samurainstein <eric.matelyan@gmail.com>
2021-08-28 10:07:40 -0400

    Setup CourseDAO class.  Create insert, update, delete, and select queries.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/CourseDAO.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/CourseDAO.java
@@ -1,4 +1,29 @@
 package com.ericmatelyan_schoolmobileapp.DAO;
 
+import static androidx.room.OnConflictStrategy.IGNORE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
+
+import java.util.List;
+
+@Dao
 public interface CourseDAO {
+    @Insert(onConflict = IGNORE)
+    void insert(CourseEntity course);
+
+    @Update
+    void update(CourseEntity course);
+
+    @Delete
+    void delete(CourseEntity course);
+
+    @Query("SELECT * FROM course_table")
+    List<CourseEntity> getAllCourses();
 }
