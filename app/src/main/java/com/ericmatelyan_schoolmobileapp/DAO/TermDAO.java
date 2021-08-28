163abcb2e52533f3f63be6bd11fe50e9ee0ca432
samurainstein <eric.matelyan@gmail.com>
2021-08-28 10:07:53 -0400

    Setup TermDAO class.  Create insert, update, delete, and select queries.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/TermDAO.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/DAO/TermDAO.java
@@ -1,4 +1,29 @@
 package com.ericmatelyan_schoolmobileapp.DAO;
 
+import static android.icu.text.MessagePattern.ArgType.SELECT;
+import static androidx.room.OnConflictStrategy.IGNORE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
+
+import java.util.List;
+
+@Dao
 public interface TermDAO {
+    @Insert(onConflict = IGNORE)
+    void insert(TermEntity term);
+
+    @Update
+    void update(TermEntity term);
+
+    @Delete
+    void delete(TermEntity term);
+
+    @Query("SELECT * FROM term_table")
+    List<TermEntity> getAllTerms();
 }
