80b2a3e0b06e1aede25975629c01c90c467932a0
samurainstein <eric.matelyan@gmail.com>
2021-08-29 13:39:41 -0400

    Initial setup of DB class.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/Database/SchoolCalendarDB.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/Database/SchoolCalendarDB.java
@@ -1,4 +1,40 @@
 package com.ericmatelyan_schoolmobileapp.Database;
 
-public class SchoolCalendarDB {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.ericmatelyan_schoolmobileapp.DAO.AssignmentDAO;
+import com.ericmatelyan_schoolmobileapp.DAO.CourseDAO;
+import com.ericmatelyan_schoolmobileapp.DAO.TermDAO;
+import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
+import com.ericmatelyan_schoolmobileapp.Utility.DateConverter;
+
+@Database(entities = {TermEntity.class, CourseEntity.class, AssignmentEntity.class}, version = 1, exportSchema = false)
+@TypeConverters({DateConverter.class})
+public abstract class SchoolCalendarDB extends RoomDatabase {
+
+    public abstract TermDAO termDAO();
+    public abstract CourseDAO courseDAO();
+    public abstract AssignmentDAO assignmentDAO();
+
+    private static volatile SchoolCalendarDB INSTANCE;
+
+    static SchoolCalendarDB getDatabase(final Context context) {
+        if(INSTANCE==null) {
+            synchronized (SchoolCalendarDB.class) {
+                if(INSTANCE==null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), SchoolCalendarDB.class, "School Calendar DB")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
