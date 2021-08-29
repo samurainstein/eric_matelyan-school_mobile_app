edef2573a6cc72b7c3b63502c9fe8985e3827a7c
samurainstein <eric.matelyan@gmail.com>
2021-08-29 19:51:59 -0400

    Modified date conversions to LocalDate <=> String

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/Utility/DateConverter.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/Utility/DateConverter.java
@@ -2,18 +2,36 @@ package com.ericmatelyan_schoolmobileapp.Utility;
 
 import androidx.room.TypeConverter;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-public class DateConverter {
+public abstract class DateConverter {
+//    private static String pattern = "dd-MM-yyyy";
+//    private static SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private static LocalDate localDate;
+    private static String stringDate;
+
     @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value);
+    public static LocalDate stringToDate(String date) {
+        if(date == null)
+            localDate = null;
+        else if(date != null) {
+            localDate = LocalDate.parse(date);
+
+        }
+        return localDate;
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
+    public static String dateToString(LocalDate date) {
+        if(date == null)
+            stringDate = null;
+        else if(date != null) {
+            stringDate = date.format(formatter);
+        }
+        return stringDate;
     }
-
 }
