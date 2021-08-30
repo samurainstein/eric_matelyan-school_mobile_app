08a29eea2bde434af631c9a9ca34e48055d476cd
samurainstein <eric.matelyan@gmail.com>
2021-08-30 13:57:11 -0400

    Create Courses Activity

---
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/CoursesActivity.java
@@ -0,0 +1,16 @@
+package com.ericmatelyan_schoolmobileapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ericmatelyan_schoolmobileapp.R;
+
+public class CoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
