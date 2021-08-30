f61eba3b9cc65033d6846404c464df5d29386f62
samurainstein <eric.matelyan@gmail.com>
2021-08-30 13:56:40 -0400

    Create Terms Activity

---
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermsActivity.java
@@ -0,0 +1,16 @@
+package com.ericmatelyan_schoolmobileapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ericmatelyan_schoolmobileapp.R;
+
+public class TermsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+    }
+}
