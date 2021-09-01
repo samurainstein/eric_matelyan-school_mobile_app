76a369fe12a6abcceda63f7fed20e53bfa8a500d
samurainstein <eric.matelyan@gmail.com>
2021-09-01 13:21:15 -0400

    Add menu bar with home icon.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermsDetailActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermsDetailActivity.java
@@ -2,7 +2,10 @@ package com.ericmatelyan_schoolmobileapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.ericmatelyan_schoolmobileapp.R;
@@ -19,5 +22,24 @@ public class TermsDetailActivity extends AppCompatActivity {
 
         termName = getIntent().getStringExtra("termName");
         editName = findViewById(R.id.term_details_edit);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_menu_item:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
