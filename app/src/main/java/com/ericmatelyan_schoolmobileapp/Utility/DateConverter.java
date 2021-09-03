f64332add8b690249e7cd6dc3f989d1b055ec17c
samurainstein <eric.matelyan@gmail.com>
2021-09-03 13:18:32 -0400

    Changed Date Converter to use Date, instead of LocalDate.  Fixed crashing issue when clicking on Terms button.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/Utility/DateConverter.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/Utility/DateConverter.java
@@ -2,36 +2,15 @@ package com.ericmatelyan_schoolmobileapp.Utility;
 
 import androidx.room.TypeConverter;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public abstract class DateConverter {
-//    private static String pattern = "dd-MM-yyyy";
-//    private static SimpleDateFormat formatter = new SimpleDateFormat(pattern);
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    private static LocalDate localDate;
-    private static String stringDate;
-
     @TypeConverter
-    public static LocalDate stringToDate(String date) {
-        if(date == null)
-            localDate = null;
-        else if(date != null) {
-            localDate = LocalDate.parse(date);
-
-        }
-        return localDate;
+    public static Date toDate(Long timestamp) {
+        return timestamp == null ?  null : new Date(timestamp);
     }
-
     @TypeConverter
-    public static String dateToString(LocalDate date) {
-        if(date == null)
-            stringDate = null;
-        else if(date != null) {
-            stringDate = date.format(formatter);
-        }
-        return stringDate;
+    public static Long toTimestamp(Date date) {
+        return date == null ? null : date.getTime();
     }
 }
