c9862a5c1ffa33619e21c39058466fa56e2fc1a0
samurainstein <eric.matelyan@gmail.com>
2021-09-04 18:55:09 -0400

    Set Views using Intent Extras from TermsActivity

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermsDetailActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermsDetailActivity.java
@@ -6,14 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ericmatelyan_schoolmobileapp.R;
 
 public class TermsDetailActivity extends AppCompatActivity {
 
     private String termName;
-    EditText editName;
+    private String startDate;
+    private String endDate;
+    private TextView titleText;
+    private TextView startText;
+    private TextView endText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,21 @@ public class TermsDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_terms_detail);
 
         termName = getIntent().getStringExtra("termName");
-        editName = findViewById(R.id.term_details_edit);
+        startDate = getIntent().getStringExtra("startDate");
+        endDate = getIntent().getStringExtra("endDate");
+
+        titleText = findViewById(R.id.term_details_title_text);
+        startText = findViewById(R.id.term_details_start_text);
+        endText = findViewById(R.id.term_details_end_text);
+
+        titleText.setText(termName);
+        startText.setText(startDate);
+        endText.setText(endDate);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.editmenu, menu);
         return true;
     }
 
@@ -34,8 +47,16 @@ public class TermsDetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.home_menu_item:
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+
+            case R.id.edit_menu_item:
+                Intent editIntent = new Intent(this, EditTermActivity.class);
+                editIntent.putExtra("termName", termName);
+                editIntent.putExtra("startDate", startDate);
+                editIntent.putExtra("endDate", endDate);
+                startActivity(editIntent);
                 return true;
 
             default:
