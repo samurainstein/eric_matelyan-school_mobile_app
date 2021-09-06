7c37b972c1504d38410e9795f1fe121a4ddac26f
samurainstein <eric.matelyan@gmail.com>
2021-09-06 10:05:41 -0400

    Clean up code

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/CoursesActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/CoursesActivity.java
@@ -1,18 +1,15 @@
 package com.ericmatelyan_schoolmobileapp.UI;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
 import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
 import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
-import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
 import com.ericmatelyan_schoolmobileapp.R;
-import com.ericmatelyan_schoolmobileapp.Utility.IdManager;
 
-import java.util.Date;
 import java.util.List;
 
 public class CoursesActivity extends AppCompatActivity {
@@ -26,17 +23,17 @@ public class CoursesActivity extends AppCompatActivity {
 
         repository = new SchoolCalendarRepo(getApplication());
         List<CourseEntity> allCourses = repository.getAllCourses();
-        if(allCourses.size()==0) {
-            CourseEntity course = new CourseEntity(IdManager.getNextCourseId(),
-                    "Test Course",
-                    new Date(),
-                    new Date(),
-                    "Status",
-                    "Instructor Name",
-                    "Instructor Phone",
-                    "Instructor Email");
-            repository.insert(course);
-        }
+//        if(allCourses.size()==0) {
+//            CourseEntity course = new CourseEntity(IdManager.getNextCourseId(),
+//                    "Test Course",
+//                    new Date(),
+//                    new Date(),
+//                    "Status",
+//                    "Instructor Name",
+//                    "Instructor Phone",
+//                    "Instructor Email");
+//            repository.insert(course);
+//        }
 
         RecyclerView recyclerView = findViewById(R.id.courses_recycler);
         final CourseAdapter courseAdapter = new CourseAdapter(this);
