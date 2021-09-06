9caadc224c48a057944d537e7a518db177a4b47e
samurainstein <eric.matelyan@gmail.com>
2021-09-06 10:04:30 -0400

    Clean up code

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermAdapter.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/TermAdapter.java
@@ -8,17 +8,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
 import com.ericmatelyan_schoolmobileapp.R;
 import com.ericmatelyan_schoolmobileapp.Utility.DateConverter;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder> {
 
@@ -45,7 +41,7 @@ public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder
 
                     startDateText = DateConverter.dateToString(current.getStartDate());
                     endDateText = DateConverter.dateToString(current.getEndDate());
-                    Intent intent = new Intent(context, TermsDetailActivity.class);
+                    Intent intent = new Intent(context, TermDetailsActivity.class);
                     intent.putExtra("termId", current.getTermId());
                     intent.putExtra("termName", current.getTermName());
                     intent.putExtra("startDate", startDateText);
