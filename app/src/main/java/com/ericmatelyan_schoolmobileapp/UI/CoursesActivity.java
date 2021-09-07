ddee01b9b4130b75eae1a84f12dcbdbe7743e15e
samurainstein <eric.matelyan@gmail.com>
2021-09-06 22:20:01 -0400

    Uncomment code for inital Course Entity setup

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/CoursesActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/CoursesActivity.java
@@ -9,7 +9,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
 import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
 import com.ericmatelyan_schoolmobileapp.R;
+import com.ericmatelyan_schoolmobileapp.Utility.IdManager;
 
+import java.util.Date;
 import java.util.List;
 
 public class CoursesActivity extends AppCompatActivity {
@@ -23,17 +25,18 @@ public class CoursesActivity extends AppCompatActivity {
 
         repository = new SchoolCalendarRepo(getApplication());
         List<CourseEntity> allCourses = repository.getAllCourses();
-//        if(allCourses.size()==0) {
-//            CourseEntity course = new CourseEntity(IdManager.getNextCourseId(),
-//                    "Test Course",
-//                    new Date(),
-//                    new Date(),
-//                    "Status",
-//                    "Instructor Name",
-//                    "Instructor Phone",
-//                    "Instructor Email");
-//            repository.insert(course);
-//        }
+        if(allCourses.size()==0) {
+            CourseEntity course = new CourseEntity(IdManager.getNextCourseId(),
+                    "Test Course",
+                    new Date(),
+                    new Date(),
+                    "Status",
+                    "Instructor Name",
+                    "Instructor Phone",
+                    "Instructor Email",
+                    "Term 1");
+            repository.insert(course);
+        }
 
         RecyclerView recyclerView = findViewById(R.id.courses_recycler);
         final CourseAdapter courseAdapter = new CourseAdapter(this);
