d7b92598eedbd9056950254d46c69cf80f7a3f73
samurainstein <eric.matelyan@gmail.com>
2021-09-09 12:28:42 -0400

    Added code to populate an initial term, course, and assignment

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/MainActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/MainActivity.java
@@ -7,22 +7,62 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
+import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
 import com.ericmatelyan_schoolmobileapp.Entity.TermEntity;
 import com.ericmatelyan_schoolmobileapp.R;
 import com.ericmatelyan_schoolmobileapp.Utility.IdManager;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SchoolCalendarRepo repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        TermEntity term1 = new TermEntity(IdManager.getNextTermId(),  "Term 1", testDate, testDate);
-//        repository.insert(term1);
+
+        repository = new SchoolCalendarRepo(getApplication());
+        List<TermEntity> allTerms = repository.getAllTerms();
+        List<CourseEntity> allCourses = repository.getAllCourses();
+        List<AssignmentEntity> allAssignments = repository.getAllAssignments();
+
+        if(allTerms.size()==0) {
+            TermEntity term = new TermEntity(IdManager.getNextTermId(),
+                    "Test Term 1",
+                    new Date(),
+                    new Date());
+            repository.insert(term);
+        }
+
+        if(allCourses.size()==0) {
+            CourseEntity course = new CourseEntity(IdManager.getNextCourseId(),
+                    "Test Course 1",
+                    new Date(),
+                    new Date(),
+                    "In Progress",
+                    "Instructor Name",
+                    "Instructor Phone",
+                    "Instructor Email",
+                    "Test Term 1",
+                    "Test notes");
+            repository.insert(course);
+        }
+
+        if(allAssignments.size()==0) {
+            AssignmentEntity assignment = new AssignmentEntity(
+                    IdManager.getNextAssignmentId(),
+                    "Test Assignment 1",
+                    "Test Course 1",
+                    "Performance Assessment",
+                    new Date(),
+                    new Date());
+            repository.insert(assignment);
+        }
     }
 
     public void termsButton(View view) {
