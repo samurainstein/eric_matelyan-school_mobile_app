9f503918c1006a855b8a0bfa44ecddb2ccc75b16
samurainstein <eric.matelyan@gmail.com>
2021-09-09 11:06:01 -0400

    Create code to generate initial Assignment.  Setup Recycler view. Define addAssignment Method.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
@@ -1,16 +1,54 @@
 package com.ericmatelyan_schoolmobileapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
+import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
 import com.ericmatelyan_schoolmobileapp.R;
+import com.ericmatelyan_schoolmobileapp.Utility.IdManager;
+
+import java.util.Date;
+import java.util.List;
 
 public class AssignmentsActivity extends AppCompatActivity {
+
+    private SchoolCalendarRepo repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assignments);
+        setContentView(R.layout.activity_courses);
+
+        repository = new SchoolCalendarRepo(getApplication());
+        List<AssignmentEntity> allAssignments = repository.getAllAssignments();
+        if(allAssignments.size()==0) {
+            AssignmentEntity assignment = new AssignmentEntity(
+                    IdManager.getNextAssignmentId(),
+                    "Performance Assessment",
+                    "Test Assignment",
+                    new Date(),
+                    new Date());
+            repository.insert(assignment);
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.courses_recycler);
+        final AssignmentAdapter assignmentAdapter = new AssignmentAdapter(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(assignmentAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        assignmentAdapter.setAssignments(allAssignments);
+    }
+
+    public void addAssignment(View view) {
+        Intent intent = new Intent(this, AddCourseActivity.class);
+        startActivity(intent);
+
     }
 }
