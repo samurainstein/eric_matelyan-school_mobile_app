82a04cc6c3fb014896166a957d9a4270767a90db
samurainstein <eric.matelyan@gmail.com>
2021-09-10 20:59:54 -0400

    Override dispatchTouchEvent() method to make the soft keyboard close when clicking on another area of the screen

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/EditTermActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/EditTermActivity.java
@@ -2,8 +2,12 @@ package com.ericmatelyan_schoolmobileapp.UI;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -104,5 +108,24 @@ public class EditTermActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TermDetailsActivity.class);
         intent.putExtra("term", updateTerm);
         startActivity(intent);
+    }
+
+    //To make the soft keyboard disappear when clicking outside of EditText
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    Log.d("focus", "touchevent");
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
