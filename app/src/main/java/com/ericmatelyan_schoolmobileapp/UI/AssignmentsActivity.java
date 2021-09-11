98102918bd1cf996e166ef19ae874cc21272dbd5
samurainstein <eric.matelyan@gmail.com>
2021-09-10 21:01:03 -0400

    Fix variables that were named incorrectly

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
@@ -1,20 +1,17 @@
 package com.ericmatelyan_schoolmobileapp.UI;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
 import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
-import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
 import com.ericmatelyan_schoolmobileapp.R;
-import com.ericmatelyan_schoolmobileapp.Utility.IdManager;
 
-import java.util.Date;
 import java.util.List;
 
 public class AssignmentsActivity extends AppCompatActivity {
@@ -24,13 +21,13 @@ public class AssignmentsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses);
+        setContentView(R.layout.activity_assignments);
 
         repository = new SchoolCalendarRepo(getApplication());
         List<AssignmentEntity> allAssignments = repository.getAllAssignments();
 
 
-        RecyclerView recyclerView = findViewById(R.id.courses_recycler);
+        RecyclerView recyclerView = findViewById(R.id.assignments_recycler);
         final AssignmentAdapter assignmentAdapter = new AssignmentAdapter(this);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setAdapter(assignmentAdapter);
@@ -39,7 +36,7 @@ public class AssignmentsActivity extends AppCompatActivity {
     }
 
     public void addAssignment(View view) {
-        Intent intent = new Intent(this, AddCourseActivity.class);
+        Intent intent = new Intent(this, AddAssignmentActivity.class);
         startActivity(intent);
 
     }
