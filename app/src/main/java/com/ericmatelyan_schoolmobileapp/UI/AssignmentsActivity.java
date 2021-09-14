429b968cab774bbcca8653405a8a53ecc3c64973
samurainstein <eric.matelyan@gmail.com>
2021-09-14 12:16:35 -0400

    Add code to make sure a course exists before adding an assignment.

--- app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
+++ app/src/main/java/com/ericmatelyan_schoolmobileapp/UI/AssignmentsActivity.java
@@ -1,8 +1,10 @@
 package com.ericmatelyan_schoolmobileapp.UI;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ericmatelyan_schoolmobileapp.Database.SchoolCalendarRepo;
 import com.ericmatelyan_schoolmobileapp.Entity.AssignmentEntity;
+import com.ericmatelyan_schoolmobileapp.Entity.CourseEntity;
 import com.ericmatelyan_schoolmobileapp.R;
 
 import java.util.List;
@@ -17,11 +20,14 @@ import java.util.List;
 public class AssignmentsActivity extends AppCompatActivity {
 
     private SchoolCalendarRepo repository;
+    Context context;
+    private List<CourseEntity> allCourses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignments);
+        context = AssignmentsActivity.this;
 
         repository = new SchoolCalendarRepo(getApplication());
         List<AssignmentEntity> allAssignments = repository.getAllAssignments();
@@ -36,8 +42,17 @@ public class AssignmentsActivity extends AppCompatActivity {
     }
 
     public void addAssignment(View view) {
-        Intent intent = new Intent(this, AddAssignmentActivity.class);
-        startActivity(intent);
+        allCourses = repository.getAllCourses();
+        if(allCourses.isEmpty()) {
+            CharSequence text = "Please create a course before creating an assignment";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
 
+        else {
+            Intent intent = new Intent(this, AddAssignmentActivity.class);
+            startActivity(intent);
+        }
     }
 }
